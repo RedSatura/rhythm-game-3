12d2bd97855d702d18b879f4aeb047ec721bcde4
RedSatura <redsatura2147@gmail.com>
2024-05-01 14:36:17 +0800

    change stretch mode, fix screen touch not being detected

--- scenes/title/title_screen.gd
+++ scenes/title/title_screen.gd
@@ -2,5 +2,8 @@ extends Node2D
 
 @onready var status_label = $StatusLabel
 
+func _ready():
+	OS.request_permissions()
+
 func _on_song_file_picker_file_selected(path):
 	status_label.text = "Loading %s..." % [path]
