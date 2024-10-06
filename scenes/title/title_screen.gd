485e5496611c734ca5f9a17e90734b784ff0b898
RedSatura <redsatura2147@gmail.com>
2024-10-06 10:54:45 +0800

    fix preview not working on export

--- scenes/title/title_screen.gd
+++ scenes/title/title_screen.gd
@@ -49,7 +49,11 @@ func message_received(message: String) -> void:
 	message_display.update_message(0, message)
 	
 func song_validated() -> void:
-	song_previewer.stream = AudioStreamOggVorbis.load_from_file(GlobalData.song_info["audio_src"])
+	if FileAccess.file_exists(GlobalData.song_info["audio_src"]):
+		song_previewer.stream = AudioStreamOggVorbis.load_from_file(GlobalData.song_info["audio_src"])
+	elif ResourceLoader.exists(GlobalData.song_info["audio_src"]):
+		song_previewer.stream = ResourceLoader.load(GlobalData.song_info["audio_src"])
+	
 	song_previewer.play()
 	
 func reset_to_defaults() -> void:
