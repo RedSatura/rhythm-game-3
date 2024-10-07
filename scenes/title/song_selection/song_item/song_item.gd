f3ca165f7a213be2df7946ab501d5a0e2d9f6859
RedSatura <redsatura2147@gmail.com>
2024-10-07 18:12:44 +0800

    add scrolling to long artist names

--- scenes/title/song_selection/song_item/song_item.gd
+++ scenes/title/song_selection/song_item/song_item.gd
@@ -9,10 +9,16 @@ extends Button
 @onready var title_scroll_timer: Node = $TitleScroll/Timer
 @onready var title_scroll_h: Node = $TitleScroll.get_h_scroll_bar()
 @onready var title_label: Node = $TitleScroll/SongTitle
-@onready var artist_label: Node = $ArtistName
+
+@onready var artist_scroll: Node = $ArtistScroll
+@onready var artist_scroll_timer: Node = $ArtistScroll/ArtistScrollTimer
+@onready var artist_scroll_h: Node = $ArtistScroll.get_h_scroll_bar()
+@onready var artist_label: Node = $ArtistScroll/ArtistName
+
 @onready var difficulty_label: Node = $Difficulty
 
 var scrolling_enabled: bool = false
+var artist_scrolling_enabled: bool = true
 var on_focus: bool = false
 
 enum ScrollState {
@@ -21,7 +27,14 @@ enum ScrollState {
 	END,
 }
 
+enum ArtistScrollState {
+	BEGIN,
+	MOVING,
+	END,
+}
+
 var scroll_state: int = ScrollState.BEGIN
+var artist_scroll_state: int = ArtistScrollState.BEGIN
 
 func _ready() -> void:
 	SignalHandler.connect("song_validated", Callable(self, "song_validated"))
@@ -29,6 +42,7 @@ func _ready() -> void:
 	artist_label.text = song_artist
 	difficulty_label.text = "Difficulty: " + str(song_difficulty)
 	title_scroll_timer.start()
+	artist_scroll_timer.start()
 
 func _on_pressed() -> void:
 	SignalHandler.emit_signal("send_song_to_validator", song_path)
@@ -47,6 +61,17 @@ func _process(_delta: float) -> void:
 				title_scroll_timer.start()
 				scroll_state = ScrollState.END
 		ScrollState.END:
+			pass
+			
+	match artist_scroll_state:
+		ArtistScrollState.BEGIN:
+			pass
+		ArtistScrollState.MOVING:
+			artist_scroll.scroll_horizontal += 1
+			if artist_scroll.scroll_horizontal + 176 >= artist_scroll_h.max_value:
+				artist_scroll_timer.start()
+				artist_scroll_state = ArtistScrollState.END
+		ArtistScrollState.END:
 			pass
 
 func _on_timer_timeout() -> void:
@@ -68,3 +93,15 @@ func _on_focus_entered() -> void:
 
 func _on_focus_exited() -> void:
 	on_focus = false
+
+func _on_artist_scroll_timer_timeout() -> void:
+	match artist_scroll_state:
+		ArtistScrollState.BEGIN:
+			artist_scrolling_enabled = true
+			artist_scroll_state = ArtistScrollState.MOVING
+		ArtistScrollState.MOVING:
+			pass
+		ArtistScrollState.END:
+			artist_scroll_timer.start()
+			artist_scroll.scroll_horizontal = 0
+			artist_scroll_state = ArtistScrollState.BEGIN
